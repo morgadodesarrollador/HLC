4c07d945fad98c264e3cdf718cf4205b0bca7c9e
Morgado <morgadodesarrollador@gmail.com>
2021-03-12 14:01:08 +0100

    rol apache2 12/03

---
+++ Ansible/proyectos/web/roles/apache2/files/composer.sh
@@ -0,0 +1,3 @@
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer 
+chmod +x /usr/local/bin/composer
---
+++ Ansible/proyectos/web/roles/hosts/files/addhosts.sh
@@ -0,0 +1,2 @@
+echo "172.130.10.1 router.asir.es" >> /etc/hosts
+echo "172.130.10.2 sweb1.asir.es" >> /etc/hosts
