f5964bd87fc9953f6bd16e410d412bde50e97526
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-01-12 20:27:03 +0100

    reajuste base

--- Docker/dockerfiles/ubuntu/apachePhp/start-php.sh
+++ Docker/dockerfiles/ubuntu/apachePhp/start-php.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-
+set -e
 bash /root/start.sh
 
-set -e
+config_apache(){
+    #sed -i "s/\${APACHE_RUN_USER}/\www-data/g" /etc/apache2/apache2.conf
+    #sed -i "s/${APACHE_RUN_DIR}/$/g" /etc/apache2/apache2.conf
+    #asocia las variables del apache2.conf con los valores definidos en /etc/apache2/envars
+    source /etc/apache2/envvars
+    # con apache2 -S veremos los valores del las variables asociadas
 
+    # Apache gets grumpy about PID files pre-existing
+    #rm -f /var/run/apache2/apache2.pid
+}
+
+main() {
+    config_apache
+    /usr/sbin/apache2 -DFOREGROUND
+}
+
+main
